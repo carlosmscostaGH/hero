3151e47edf50a14a07ad078007bb1400849af55a
Carlos Costa <up202004151@up.pt>
2022-10-03 10:40:34 +0100

    Adding Key Recognition

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -8,7 +10,9 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
-    private TerminalScreen screen;
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
     public Game() throws IOException {
         try {
             Terminal terminal = new
@@ -26,10 +30,19 @@ public class Game {
     }
     private void draw() throws IOException{
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
     public void run() throws IOException{
         draw();
+        KeyStroke key = screen.readInput();
+        processKey(key);
+    }
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+        if (key.getKeyType() == KeyType.ArrowLeft) x-= 1;
+        if (key.getKeyType() == KeyType.ArrowRight) x+= 1;
+        if (key.getKeyType() == KeyType.ArrowUp) y-= 1;
+        if (key.getKeyType() == KeyType.ArrowDown) y+= 1;
     }
 }
